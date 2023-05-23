08083a5414472fbd2abd785d5b757b2db4fccfca
stephanef <stephanef@imagemattersllc.com>
2023-05-23 15:43:40 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,138 @@
+var respecConfig = {
+  specStatus: "base",
+  logos: [],
+  editors: [{
+    name: "Open Government Data",
+  }, {
+    name: "S. Fellah",
+    company:"Image Matters LLC",
+    companyURL:"https://www.imagemattersllc.com",
+    note: "",
+  }, {
+    name:"J. Davidson",
+    company:"Image Matters LLC",
+    companyURL:"https://www.imagemattersllc.com",
+    note: "",
+  }, {
+    name:"Michael Radcliffe",
+    company:"US Census Bureau",
+    companyURL: "https://www.census.gov",
+    note: "",
+  }],
+  github: {
+    branch: "main",
+    repoURL: "https://github.com/DCAT-US/DCAT-US-Schema-v3.0",
+  },
+  edDraftURI: "https://github.com/DCAT-US/DCAT-US-Schema-v3.0/dcat-us.html",
+  subtitle: "A DCAT Application Profile for United States Of America (Working Draft)",
+  localBiblio: {
+    "DATA-GOV":{
+      "href": "https://data.gov/",
+      "title": "data.gov - The official portal for US Data",
+      "publisher": "OMB?"
+    },
+    "DCAT-US-GITHUB-ISSUES":{
+      "href":"https://github.com/DCAT-US/DCAT-US-Schema-v3.0/issues",
+      "title": "GitHub issues for DCAT-US Version 3.0",
+      "publisher": "https://github.com/DCAT-US"
+    },   
+    "FAIR" : {
+        title : "The FAIR Guiding Principles for scientific data management and stewardship",
+        authors : [ "Mark D. Wilkinson" ],
+        etAl : true,
+        status : "Scientific Data, vol. 3, Article nr. 160018",
+        publisher : "Nature",
+        href : "https://doi.org/10.1038/sdata.2016.18"
+    },
+    "GEONAMES":{
+      "href":"http://sws.geonames.org/",
+      "title":"GeoNames",
+      "publisher":"Geonames",
+    },
+    "ISO 639-1":{
+      "href":"https://en.wikipedia.org/wiki/ISO_639-1",
+      "title":"Language 2 Letter Code",
+      "publisher":"Wikipedia",
+    },
+    "PROV":{
+      "href":"http://www.w3.org/ns/prov/",
+      "title":"The PROV Namespace",
+      "publisher":"Provenance Working Group",
+    },
+    "SPDX":{
+      "href":"https://spdx.org/rdf/terms/",
+      "title":"SPDX 2.2.1",
+      "publisher":"The Linux Foundation",
+    },
+    "RESPEC":{
+      "href": "https://respec.org/docs/",
+      "title": "Respec.org",
+      "publisher": "W3C",
+    },
+    "VOCAB-US-LICENSE":{
+      "href":"https://dcat-us/vocabulary/licenses",
+      "title":"List of licenses for US opendata to enter in dct:license",
+      "publisher":"??",
+    },
+    "VOCAB-CH-THEME":{
+      "href":"http://dcat-us/vocabulary/themes",
+      "title":"Themes vocabulary for DCAT-US",
+      "publisher":"",
+    },
+    "VOCAB-EU":{
+       "href":"http://publications.europa.eu/resource/authority/",
+      "title":"EU Vocabularies",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-ACCESS-RIGHT":{
+      "href":"http://publications.europa.eu/resource/authority/access-right",
+      "title":"Access Rights Named Authority List",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-AVAILABLITY":{
+      "href":"http://publications.europa.eu/resource/authority/planned-availability",
+      "title":"Distribution availability vocabulary",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-CONTINENT":{
+      "href":"http://publications.europa.eu/resource/authority/continent",
+      "title":"EU Vocabularies Continents Named Authority List",
+      "publisher":"Publications Office of the European Union",
+     },
+    "VOCAB-EU-COUNTRY":{
+      "href":"https://publications.europa.eu/resource/authority/country",
+      "title":"EU Vocabularies Countries Named Authority List",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-FILE-TYPE":{
+      "href":"http://publications.europa.eu/resource/authority/file-type",
+      "title":"EU Vocabularies File Type Named Authority List",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-FREQUENCY":{
+      "href":"http://publications.europa.eu/resource/authority/frequency",
+      "title":"EU Vocabularies Frequency Named Authority List",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-LANGUAGE":{
+      "href":"http://publications.europa.eu/resource/authority/language",
+      "title":"Continent",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-PLACE":{
+      "href":"https://publications.europa.eu/resource/authority/place",
+      "title":"Vocabularies Places Named Authority List",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-THEME":{
+      "href":"https://publications.europa.eu/resource/authority/data-theme",
+      "title":"Dataset Theme Vocabulary",
+      "publisher":"Publications Office of the European Union",
+    },
+    "VOCAB-EU-OP-CONTACT":{
+      "href":"https://op.europa.eu/en/web/about-us/contact-us",
+      "title":"EU Vocabularies",
+      "publisher":"Publications Office of the European Union",
+    },
+  }
+};
