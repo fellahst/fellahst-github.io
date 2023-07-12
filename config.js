7914541599c8471be05055099cff4c9e61ba7e2a
stephanef <stephanef@imagemattersllc.com>
2023-07-12 17:31:42 -0400

    - Added section about Controlled vocabularies - Added section about Attribution - Fixed and added references

--- config.js
+++ config.js
@@ -2,35 +2,35 @@ var respecConfig = {
   specStatus: "base",
   logos: [],
   editors: [
- 
-  {
-    name:"Thomas Dabolt",
-    company:"US Department of Interior",
-    companyURL: "https://www.doi.gov",
-    note: "",
-  },
-  {
-    name:"Michael Ratcliffe",
-    company:"US Census Bureau",
-    companyURL: "https://www.census.gov",
-    note: "",
-  },
-  {
-    name: "Open Government Data",
-    company:"Data.gov",
-    companyURL: "https://www.data.gov",
-  }, 
-  {
-    name: "Stephane Fellah",
-    company:"Image Matters LLC",
-    companyURL:"https://www.imagemattersllc.com",
-    note: "",
-  }, {
-    name:"John Davidson",
-    company:"Image Matters LLC",
-    companyURL:"https://www.imagemattersllc.com",
-    note: "",
-  }, ],
+
+    {
+      name: "Thomas Dabolt",
+      company: "US Department of Interior",
+      companyURL: "https://www.doi.gov",
+      note: "",
+    },
+    {
+      name: "Michael Ratcliffe",
+      company: "US Census Bureau",
+      companyURL: "https://www.census.gov",
+      note: "",
+    },
+    {
+      name: "Open Government Data",
+      company: "Data.gov",
+      companyURL: "https://www.data.gov",
+    },
+    {
+      name: "Stephane Fellah",
+      company: "Image Matters LLC",
+      companyURL: "https://www.imagemattersllc.com",
+      note: "",
+    }, {
+      name: "John Davidson",
+      company: "Image Matters LLC",
+      companyURL: "https://www.imagemattersllc.com",
+      note: "",
+    },],
   github: {
     branch: "main",
     repoURL: "https://github.com/DOI-DO/dcat-us/",
@@ -38,118 +38,141 @@ var respecConfig = {
   edDraftURI: "https://github.com/DOI-DO/dcat-us/dcat-us.html",
   subtitle: "A DCAT Application Profile for United States Of America (Working Draft)",
   localBiblio: {
-    "DATA-GOV":{
+    "CC": {
+      "href": "http://creativecommons.org/licenses/",
+      "title": "About The Licenses",
+      "publisher": "Creative Commons"
+    },
+    "CC0": {
+      "href": "http://creativecommons.org/publicdomain/zero/1.0/",
+      "title": "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+      "publisher": "Creative Commons"
+    },
+    "CC-BY": {
+      "href": "https://creativecommons.org/licenses/by/4.0/",
+      "title": "CC-BY 4.0 Attribution 4.0 International",
+      "publisher": "Creative Commons"
+    },
+    "PDDL": {
+      "href": "http://opendatacommons.org/licenses/pddl/",
+      "title": "Public Domain Dedication and License (PDDL)",
+      "publisher": "Open Data Commons"
+    },
+
+    "DATA-GOV": {
       "href": "https://data.gov/",
       "title": "data.gov - The official portal for US Data",
       "publisher": "OMB?"
     },
-    "DCAT-US":{
-      "href":"https://github.com/DOI-DO/dcat-us/",
+    "DCAT-US": {
+      "href": "https://github.com/DOI-DO/dcat-us/",
       "title": "DCAT-US Version 3.0",
       "publisher": "https://github.com/DOI-DO"
-    },   
-    "DCAT-US-GITHUB-ISSUES":{
-      "href":"https://github.com/DOI-DO/dcat-us/issues",
+    },
+    "DCAT-US-GITHUB-ISSUES": {
+      "href": "https://github.com/DOI-DO/dcat-us/issues",
       "title": "GitHub issues for DCAT-US Version 3.0",
       "publisher": "https://github.com/DOI-DO"
-    },   
-    "FAIR" : {
-        title : "The FAIR Guiding Principles for scientific data management and stewardship",
-        authors : [ "Mark D. Wilkinson" ],
-        etAl : true,
-        status : "Scientific Data, vol. 3, Article nr. 160018",
-        publisher : "Nature",
-        href : "https://doi.org/10.1038/sdata.2016.18"
     },
-    "GEONAMES":{
-      "href":"http://sws.geonames.org/",
-      "title":"GeoNames",
-      "publisher":"Geonames",
+    "FAIR": {
+      "title": "The FAIR Guiding Principles for scientific data management and stewardship",
+      "authors": ["Mark D. Wilkinson"],
+      "etAl": true,
+      "status": "Scientific Data, vol. 3, Article nr. 160018",
+      "publisher": "Nature",
+      "href": "https://doi.org/10.1038/sdata.2016.18"
     },
-    "ISO 639-1":{
-      "href":"https://en.wikipedia.org/wiki/ISO_639-1",
-      "title":"Language 2 Letter Code",
-      "publisher":"Wikipedia",
+    "GEONAMES": {
+      "href": "http://sws.geonames.org/",
+      "title": "GeoNames",
+      "publisher": "Geonames",
     },
-    "PROV":{
-      "href":"http://www.w3.org/ns/prov/",
-      "title":"The PROV Namespace",
-      "publisher":"Provenance Working Group",
+    "GeoSPARQL11": {
+      "href": "https://opengeospatial.github.io/ogc-geosparql/geosparql11/spec.html",
+      "title": "OGC GeoSPARQL 1.1",
+      "publisher": "OGC",
+      "status": "Draft",
+      "date": "16 December 2020"
     },
-    "SPDX":{
-      "href":"https://spdx.org/rdf/terms/",
-      "title":"SPDX 2.2.1",
-      "publisher":"The Linux Foundation",
+    "ISO 639-1": {
+      "href": "https://en.wikipedia.org/wiki/ISO_639-1",
+      "title": "Language 2 Letter Code",
+      "publisher": "Wikipedia",
     },
-    "RESPEC":{
+    "PROV": {
+      "href": "http://www.w3.org/ns/prov/",
+      "title": "The PROV Namespace",
+      "publisher": "Provenance Working Group",
+    },
+    "SPDX": {
+      "href": "https://spdx.org/rdf/terms/",
+      "title": "SPDX 2.2.1",
+      "publisher": "The Linux Foundation",
+    },
+    "RESPEC": {
       "href": "https://respec.org/docs/",
       "title": "Respec.org",
       "publisher": "W3C",
     },
-    "VOCAB-US-LICENSE":{
-      "href":"https://dcat-us/vocabulary/licenses",
-      "title":"List of licenses for US opendata to enter in dct:license",
-      "publisher":"??",
+    "VOCAB-US-LICENSE": {
+      "href": "https://dcat-us/vocabulary/licenses",
+      "title": "List of licenses for US opendata to enter in dct:license",
+      "publisher": "??",
     },
-    "VOCAB-CH-THEME":{
-      "href":"http://dcat-us/vocabulary/themes",
-      "title":"Themes vocabulary for DCAT-US",
-      "publisher":"",
+    "VOCAB-EU": {
+      "href": "http://publications.europa.eu/resource/authority/",
+      "title": "EU Vocabularies",
+      "publisher": "Publications Office of the European Union",
     },
-    "VOCAB-EU":{
-       "href":"http://publications.europa.eu/resource/authority/",
-      "title":"EU Vocabularies",
-      "publisher":"Publications Office of the European Union",
+    "VOCAB-EU-ACCESS-RIGHT": {
+      "href": "http://publications.europa.eu/resource/authority/access-right",
+      "title": "Access Rights Named Authority List",
+      "publisher": "Publications Office of the European Union",
     },
-    "VOCAB-EU-ACCESS-RIGHT":{
-      "href":"http://publications.europa.eu/resource/authority/access-right",
-      "title":"Access Rights Named Authority List",
-      "publisher":"Publications Office of the European Union",
+    "VOCAB-EU-AVAILABLITY": {
+      "href": "http://publications.europa.eu/resource/authority/planned-availability",
+      "title": "Distribution availability vocabulary",
+      "publisher": "Publications Office of the European Union",
     },
-    "VOCAB-EU-AVAILABLITY":{
-      "href":"http://publications.europa.eu/resource/authority/planned-availability",
-      "title":"Distribution availability vocabulary",
-      "publisher":"Publications Office of the European Union",
+    "VOCAB-EU-CONTINENT": {
+      "href": "http://publications.europa.eu/resource/authority/continent",
+      "title": "EU Vocabularies Continents Named Authority List",
+      "publisher": "Publications Office of the European Union",
     },
-    "VOCAB-EU-CONTINENT":{
-      "href":"http://publications.europa.eu/resource/authority/continent",
-      "title":"EU Vocabularies Continents Named Authority List",
-      "publisher":"Publications Office of the European Union",
-     },
-    "VOCAB-US-COUNTRY":{
-      "href":"http://id.loc.gov/vocabulary/countries",
-      "title":"MARC List for Countries",
-      "publisher":"Library of Congress",
+    "VOCAB-US-COUNTRY": {
+      "href": "http://id.loc.gov/vocabulary/countries",
+      "title": "MARC List for Countries",
+      "publisher": "Library of Congress",
     },
-    "VOCAB-EU-FILE-TYPE":{
-      "href":"http://publications.europa.eu/resource/authority/file-type",
-      "title":"EU Vocabularies File Type Named Authority List",
-      "publisher":"Publications Office of the European Union",
+    "VOCAB-EU-FILE-TYPE": {
+      "href": "http://publications.europa.eu/resource/authority/file-type",
+      "title": "EU Vocabularies File Type Named Authority List",
+      "publisher": "Publications Office of the European Union",
     },
-    "VOCAB-EU-FREQUENCY":{
-      "href":"http://publications.europa.eu/resource/authority/frequency",
-      "title":"EU Vocabularies Frequency Named Authority List",
-      "publisher":"Publications Office of the European Union",
+    "VOCAB-EU-FREQUENCY": {
+      "href": "http://publications.europa.eu/resource/authority/frequency",
+      "title": "EU Vocabularies Frequency Named Authority List",
+      "publisher": "Publications Office of the European Union",
     },
-    "VOCAB-US-LANGUAGE":{
-      "href":"https://id.loc.gov/vocabulary/iso639-1",
-      "title":"ISO 639-1: Codes for the Representation of Names of Languages - Part 1: Two-letter codes for languages",
-      "publisher":"Library of Congress",
+    "VOCAB-US-LANGUAGE": {
+      "href": "https://id.loc.gov/vocabulary/iso639-1",
+      "title": "ISO 639-1: Codes for the Representation of Names of Languages - Part 1: Two-letter codes for languages",
+      "publisher": "Library of Congress",
     },
-    "VOCAB-EU-PLACE":{
-      "href":"https://publications.europa.eu/resource/authority/place",
-      "title":"Vocabularies Places Named Authority List",
-      "publisher":"Publications Office of the European Union",
+    "VOCAB-EU-PLACE": {
+      "href": "https://publications.europa.eu/resource/authority/place",
+      "title": "Vocabularies Places Named Authority List",
+      "publisher": "Publications Office of the European Union",
     },
-    "VOCAB-EU-THEME":{
-      "href":"https://publications.europa.eu/resource/authority/data-theme",
-      "title":"Dataset Theme Vocabulary",
-      "publisher":"Publications Office of the European Union",
+    "VOCAB-EU-THEME": {
+      "href": "https://publications.europa.eu/resource/authority/data-theme",
+      "title": "Dataset Theme Vocabulary",
+      "publisher": "Publications Office of the European Union",
     },
-    "VOCAB-EU-OP-CONTACT":{
-      "href":"https://op.europa.eu/en/web/about-us/contact-us",
-      "title":"EU Vocabularies",
-      "publisher":"Publications Office of the European Union",
+    "VOCAB-EU-OP-CONTACT": {
+      "href": "https://op.europa.eu/en/web/about-us/contact-us",
+      "title": "EU Vocabularies",
+      "publisher": "Publications Office of the European Union",
     },
   }
 };
